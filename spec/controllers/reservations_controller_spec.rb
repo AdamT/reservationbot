607fa5b88f1028b6627ba57ff193fbfa106f6c2c
AdamT <atal421@yahoo.com>
2013-10-24 23:29:25 -0400

    match with expect

--- spec/controllers/reservations_controller_spec.rb
+++ spec/controllers/reservations_controller_spec.rb
@@ -5,14 +5,14 @@ describe ReservationsController do
   describe "GET 'new'" do
     it "returns http success" do
       get 'new'
-      response.should be_success
+      expect(response).to be_success
     end
   end
 
   describe "GET 'index'" do
     it "returns http success" do
       get 'index'
-      response.should be_success
+      expect(response).to be_success
     end
   end
 
