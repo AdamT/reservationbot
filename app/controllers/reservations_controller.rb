e54301f8b879a861f42e3fab5ed4b2cda17212e9
AdamT <atal421@yahoo.com>
2013-10-26 23:10:12 -0400

    add controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,5 +1,17 @@
 class ReservationsController < ApplicationController
   def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new
+    setup_response = Reservation.setup(params)
+
+    if setup_response.is_valid?
+      redirect_to reservations_path, notice: "Reservation booked!"
+    else
+      render :new
+    end
   end
 
   def index
