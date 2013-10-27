4c12e5bae7ca430701a951502b3e1f937d3e20de
AdamT <atal421@yahoo.com>
2013-10-26 23:08:48 -0400

    scopes on table

--- app/models/table.rb
+++ app/models/table.rb
@@ -1,3 +1,10 @@
 class Table < ActiveRecord::Base
+  validates :seats, uniqueness: true
+
+  has_many :reservations
+
+  scope :most_seats,  -> { order('seats desc').first.seats }
+  scope :least_seats, -> { order('seats asc').first.seats }
+  scope :can_be_used, ->(size) { where('seats >= ?', size) }
 
 end
