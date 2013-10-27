8e11eb8baf7fb1a7178ddc999e127ac505a60126
AdamT <atal421@yahoo.com>
2013-10-26 23:07:10 -0400

    references table

--- db/migrate/20131025031723_create_reservations.rb
+++ db/migrate/20131025031723_create_reservations.rb
@@ -3,6 +3,7 @@ class CreateReservations < ActiveRecord::Migration
     create_table :reservations do |t|
       t.datetime :time
       t.integer :group_size
+      t.references :table
 
       t.timestamps
     end
