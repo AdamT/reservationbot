d0c0ad629c69d50e5c3bc76d864c60cd9e343005
AdamT <atal421@yahoo.com>
2013-10-26 23:10:31 -0400

    add model, works, needs refactoring

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,60 @@
 class Reservation < ActiveRecord::Base
+  belongs_to :table
+
+  scope :within_this_time, ->(new_time) {
+    where('time between ? and ?', new_time - 1.hour - 59.minute, new_time + 1.hour + 59.minute)
+  }
+
+  class << self
+    attr_accessor :reservation_obj
+
+    def setup(params)
+      @reservation_obj = build_reservation_object(params)
+      if reservation_obj.group_size <= Table.most_seats && is_available?(reservation_obj)
+        tables = Table.can_be_used(reservation_obj.group_size) & open_tables.sort! {|open_table| open_table.seats }
+
+        binding.pry
+        @reservation = tables.first.reservations.create(
+          time: reservation_obj.time,
+          group_size: reservation_obj.group_size
+        )
+      else
+        @reservation = Reservation.new
+        @reservation.errors[:base] << 'Group too large'
+        @reservation.errors[:base] << 'No tables available'
+      end
+
+      @reservation
+
+    end
+  end
+
+  def is_valid?
+    return false unless self.errors.empty?
+    true
+  end
+
+  def self.is_available?(reservation_obj)
+    if open_tables.count == Table.all.count
+      can_add =  true
+    elsif open_tables.count == 1 && reservation_obj.group_size <= open_tables.first.seats
+      can_add = true
+    else
+      can_add = false
+    end
+
+    can_add
+  end
+  private_class_method :is_available?
+
+  def self.open_tables
+    current_reservations = within_this_time(reservation_obj.time).all
+    current_reservations.empty? ? Table.all : Table.all - current_reservations.collect {|reservation| reservation.table }
+  end
+
+  def self.build_reservation_object(params)
+    ReservationData.new(params)
+  end
+  private_class_method :build_reservation_object
+
 end
