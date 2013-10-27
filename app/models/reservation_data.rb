8aaa51205465a71ec972176095513e3f6c15bb56
AdamT <atal421@yahoo.com>
2013-10-26 23:08:21 -0400

    refactor data clump

--- app/models/reservation_data.rb
+++ app/models/reservation_data.rb
@@ -3,11 +3,34 @@ class ReservationData
   attr_accessor :time, :group_size
 
   def initialize(params)
+    @params     = params
+    @time       = build_datetime
+    @group_size = build_group_size
+  end
 
-   @group_size = params[:reservation][:group_size].to_i
+  private
 
-    @time = Date.parse(params[:reservation][:time]) + 
-      params[:hours].to_i.hours +
-      params[:minutes].to_i.minutes
+  def build_group_size
+    params[:reservation][:group_size].to_i
+  end
+
+  def build_datetime
+    date + hours + minutes
+  end
+
+  def date
+    Date.parse(params[:reservation][:time])
+  end
+
+  def hours
+    params[:hours].to_i.hours
+  end
+
+  def minutes
+    params[:minutes].to_i.minutes
+  end
+
+  def params
+    @params
   end
 end
