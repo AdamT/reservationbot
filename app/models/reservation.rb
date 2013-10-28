3101a772f7bba805435f4092e9f1b6cecc9d728d
AdamT <atal421@yahoo.com>
2013-10-27 22:20:29 -0400

    refactor scope

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,16 +2,22 @@ class Reservation < ActiveRecord::Base
   belongs_to :table
 
   default_scope order('time ASC')
+
   scope :within_this_time, ->(new_time) {
-    where(
-      'time between ? and ?',
-      new_time - 1.hour - 59.minute, new_time + 1.hour + 59.minute
-    )
+    where('time between ? and ?', earliest, latest)
   }
 
   def self.setup(params)
     @reservation_obj = ReservationData.new(params)
     ReservationSetup.new(@reservation_obj).setup
+  end
+
+  def self.earliest
+    @reservation_obj.time - 1.hour - 59.minute
+  end
+
+  def self.latest
+    @reservation_obj.time + 1.hour + 59.minute
   end
 
   def is_valid?
